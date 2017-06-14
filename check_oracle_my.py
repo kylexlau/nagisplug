c5edf7acf48519dc53784335e4b0a260df031cda
kyle lau <kylexlau@gmail.com>
2017-06-14 17:53:47 +0800

    check_oracle_my.py: add some features.

--- check_oracle_my.py
+++ check_oracle_my.py
@@ -9,16 +9,18 @@
 #  2017-05-31: rewrite the codes with argparsea lib
 #  2017-06-02: rewrite the codes using object-oriented programming
 #              and change it to pep8 style.
+#  2017-06-13: get rid of timeout option, nagios has its own timeout
+#              setting
+#  2017-06-14: add v$sysmetric monitoring
 
 ## Todo:
-# add more metrics by selecting v$sysmetrics
+#
 
 __version__ = '0.3'
 
 import os
 import re
 import sys
-import socket
 import argparse
 
 import cx_Oracle
@@ -89,41 +91,37 @@ class OracleMonitor(object):
       where ImbalancePct >= 15 or MinFreePct <= 3
     """
 
-    timeout = 10
-
-    def __init__(self, db, user, passwd, timeout):
+    def __init__(self, db, user, passwd):
         """Take the db configuration info from command line options"""
         self.db = db
         self.user = user
         self.passwd = passwd
-        if timeout is not None:
-            self.timeout = timeout
 
     def dbstat(self):
         """DBtime and active_session performance data"""
         status = 0
-        cnt_a = self.exe_sql(self.dbstat_sql)
+        cnt_a = self.exec_sql(self.dbstat_sql)
         dbtime = cnt_a[0][0]
         active_session = cnt_a[1][0]
-        msg = "check ok. | dbtime=%s;;;; active_session=%s;;;;" % \
+        msg = "Check OK. | dbtime=%s;;;; active_session=%s;;;;" % \
                 (dbtime,active_session)
         return status, msg
 
     def dbconf(self):
         """Some database configuration infomations as performance data"""
         status = 0
-        cnt_a = self.exe_sql(self.dbconf_sql)
+        cnt_a = self.exec_sql(self.dbconf_sql)
         cpu_count = cnt_a[0][0]
         sga = cnt_a[1][0]
         pga = cnt_a[2][0]
         mem = float(sga)+float(pga)
-        msg = "check ok. | cpu_count=%s;;;; sga=%s;;;; pga=%s;;;; memory=%s;;;;" \
+        msg = "Check OK. | cpu_count=%s;;;; sga=%s;;;; pga=%s;;;; memory=%s;;;;" \
                 % (cpu_count,sga,pga,str(int(round(mem))))
         return status, msg
 
     def asminb(self):
         """Check if ASM disks' data is balanced"""
-        rst = self.exe_sql(self.asm_inb_sql)
+        rst = self.exec_sql(self.asm_inb_sql)
         msg = ''
         for disk_group,imbalance_pct,min_free_pct in rst:
             s = "ASM DiskGroup %s's disks: Imblance percent is %s \
@@ -140,23 +138,54 @@ class OracleMonitor(object):
 
         return status,msg
 
-    def exe_sql(self, sql):
+    def sysmetric(self):
+        """Get v$sysmetric values"""
+        status = 0
+
+        sql = """select metric_name,value from v$sysmetric where group_id = 2
+                 and  metric_id in
+                (2003,2004,2006,2016,2018,2024,2030,2036,2046,2058,2067,2069,
+                2071,2075,2121,2109,2123,2145,2146)
+              """
+
+        rst = self.exec_sql(sql)
+        msg = "Check OK. | "
+        for metric_name, value in rst:
+            s = "%s=%s;;;; " % (metric_name.replace(' ', '_').replace('/', '').lower(), value)
+            msg += s
+        return status, msg
+
+    def exec_sql(self, sql):
         """Execute sql statement in a db, return the sql result"""
-        socket.setdefaulttimeout(self.timeout)
+        try:
+            conn = self.conn_db()
+            cur = conn.cursor()
+            cur.execute(sql)
+            return cur.fetchall()
+        except Exception as err:
+            print(err)
+            sys.exit(1)
+
+    def exec_bind_sql(self, sql, bind):
+        """Execute sql statement in a db with bind variables"""
+        try:
+            conn = self.conn_db()
+            cur = conn.cursor()
+            cur.execute(sql, **bind)
+            return cur.fetchall()
+        except Exception as err:
+            print(err)
+            sys.exit(1)
+
+    def conn_db(self):
+        """Connect to db"""
         conn_str= "%s/%s@%s" % (self.user,self.passwd,self.db)
         try:
-            conn = cx_Oracle.connect(conn_str);
-        except cx_Oracle.DatabaseError, err:
-            print "DB Error : ", err
-            sys.exit(3)
-        except socket.timeout:
-            print "Timeout !"
-            sys.exit(3)
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        rst = cursor.fetchall()
-        conn.close()
-        return rst
+            with cx_Oracle.connect(conn_str) as conn:
+                return conn
+        except Exception as err:
+            print(err)
+            sys.exit(1)
 
 def version():
     """Print version."""
@@ -170,29 +199,29 @@ def parse_args():
     parser.add_argument('-s','--database',help='Database TNS Name')
     parser.add_argument('-u','--username',help='Monitoring Username')
     parser.add_argument('-p','--password',help='Monitoring Password')
-    parser.add_argument('-t','--timeout',
-                        help='Connection Timeout (Default is 10 seconds)',
-                        default=10,type=int)
-    parser.add_argument('-m','--mode',    help="""
-           dbstat: Database statatics like active session and db time.
-           dbconf: Database configuration information like parameter values and more.
-           asminb: ASM inbalance check.""",
-                        choices=['dbstats','dbconf','asminb'])
+
+    parser.add_argument('-m','--mode',choices=['dbstats','dbconf','asminb','sysmetric'],
+                        help="dbstat: Database statatics like active session "
+                        " and db time.                                       "
+                        "dbconf: Database configuration information like     "
+                        "parameter values and more.                          "
+                        "asminb: ASM inbalance check.                        "
+                        "sysmetric: values from v$sysmetric                  " )
+
     args = parser.parse_args()
 
     if args.version:
         version()
         sys.exit(0)
-    if not args.mode or not args.username \
-       or not args.database or not args.password:
+    if args.mode is None or args.username is None\
+       or args.database is None or args.password is None:
         parser.error('DB info and mode is required.')
     return args
 
 def main():
     """The main program."""
     args = parse_args()
-    dbm = OracleMonitor(args.database, args.username,
-                        args.password, args.timeout)
+    dbm = OracleMonitor(args.database, args.username, args.password)
 
     if args.mode=='dbstats':
         return dbm.dbstat()
@@ -200,6 +229,8 @@ def main():
         return dbm.dbconf()
     elif args.mode=='asminb':
         return dbm.asminb()
+    elif args.mode=='sysmetric':
+        return dbm.sysmetric()
     else:
         print('No arguments provided.')
         sys.exit(1)
